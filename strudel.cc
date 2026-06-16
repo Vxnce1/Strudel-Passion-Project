57de48379eb584f2662aa6035c5bc588aa6f9072
Vxnce1 <vincerulloda2008@gmail.com>
2026-06-15 23:52:55 -0400

    Refactor chord definitions and note concatenation

--- strudel.cc
+++ strudel.cc
@@ -44,17 +44,17 @@ let line6 = "A4@0.5 G4@2 [D4 D4 E4]@1.5 [A4 G4 F#4]@1.5 E4 B4 A4@0.5 [G4@1.5 F#4
 
 
 
-_$: s("[bd hh hh bd rim hh hh:2 hh]").fast(1.5)
-_$: n("0 2 4 <[6,8] [7,9]>").scale("<C:major A:minor>").s("piano").fast(1.5)
+$: s("[bd hh hh bd rim hh hh:2 hh]").fast(1.5)
+  .stack(n("0 2 4 <[6,8] [7,9]>").scale("<C:major A:minor>").s("piano").fast(1.5))
 
-$: cat(
-  note(line5)
-  // note(line2),
-  // note(line3),
-  // note(line4),
-  // note(line5),
-  // note(line6),
-  // note("F#4@2, D4@2")
+_$: cat(
+   note(line1),
+  note(line2),
+  note(line3),
+  note(line4),
+  note(line5),
+  note(line6),
+  note("F#4@2, D4@2")
 )
 .slow(4)
 .sound("piano")
@@ -66,15 +66,20 @@ $: cat(
 
 let chords1 = "A3 -@3 E4 -@3 D3 -@6 E3, F3 -@3 C3 -@3 B3 -@6 C3, D3 -@3 A3 -@3 G2 -@6 A3 , A4 -@3 F2 -@3 B3 -@6 E#2"
 let chords2 = "A3 -@3 E4 -@3 G3 -@5 G3 A3, F3 -@3 C3 -@3 D3 -@5 D3 C3, D3 -@3 A3 -@3 C3 -@5 B3 E4, A4 -@3 F2 -@3 D3 -@5 D2 B3"
-let chords3 = "A4@1.5 A4@2 -@0.5 B4@1.5 B4@2 -@0.5 G3 -@3 G3 -@3, C3@1.5 C3@2 -@0.5 D4@1.5 D4@2 -@0.5 B4 -@3 B3 -@3, A3@1.5 A3@2 -@0.5 F3@1.5 F3@2 -@0.5 D4 -@3 D3 -@3, E4@1.5 E4@2 -@0.5 F2@1.5 F2@2 -@0.5 D3 -@3 D2 -@3"
-let chords4 = "[E3 E3 G3]@1.5 B3 [E3 G3 E3]@1.5 [A4 B3 D3]@1.5 F#4 [D3 F#4 E3]@1.5 A3@0.5 A4 c3 F3@0.5 G3 A4@4, B3 -@3 D2 -@7 A3@4, G3 -@3 F3 -@7 E3@4, D3 -@3 B3 -@11"
-let chords5 = "[G3 G2 B3]@1.5 G3@1.5 [D3 G2] [A4 A3 C3]@1.5 A4 [A3 A4 A3]@1.5 [G3 G2 B3]@1.5 G3@2 -@0.5 A4@2 A4 A4, [D3 - -]@1.5 D3@2.5 E3@1.5 E3@1.5 D3@1.5  D3@2 -@0.5 E3@2 F#4 E3"
+let chords3 = "A4@1.5 A4@2 -@0.5 B3@1.5 B4@2 -@0.5 G3 -@3 G3 -@3, C3@1.5 C3@2 -@0.5 D3@1.5 D3@2 -@0.5 B3 -@3 B3 -@3, A3@1.5 A3@2 -@0.5 F3@1.5 F3@2 -@0.5 D4 -@3 D3 -@3, E4@1.5 E4@2 -@0.5 F2@1.5 F2@2 -@0.5 D3 -@3 D2 -@3"
+let chords4 = "[E3 E3 G3]@1.5 B2 [E3 G3 E3]@1.5 [A4 B3 D3]@1.5 f3 [D3 F#4 E3]@1.5 A3@0.5 A4 d3 F#4@0.5 G3 A4@4, B3 -@3 D3 -@7 A3@4, G3 -@3 F2 -@7 E3@4, D3 -@3 B2 -@11"
+let chords5 = "[G3 G2 B3]@1.5 G3@1.5 [D3 G2] [A4 A3 C3]@1.5 A4 [A3 A4 A3]@1.5 [G3 G2 B3]@1.5 G3@2 -@0.5 A4@2 A4 A4, D3@1.5 D3@2.5 E3@1.5 E3@1.5 E3 D3@1.5 D3@2 -@0.5 E3@2 F#4 E3"
+  .stack("G2 -@3 A3 -@3 G2 -@3 C3@2 D3 C3, -@12 A3@2 A3 A3")
+let chords6 = "G3@1.5 G3@2 -@0.5 F3@1.5 F3@1.5 E3 [E3 E2 G2 b3]@2 [D3 F2 A3 C3]@2 [G3 G2 B3 D3]@2 A4 G3, D3@1.5 D3@2 -@0.5 C3@1.5 D3@1.5 C3 B3 - C3 - D3 - E3 D3"
+  .stack("B3@1.5 B3@2 -@0.5 A3@1.5 B3@1.5 A3 G2 - A3 - B3 - A3 G2, G2@1.5 G2@2 -@0.5 F2@1.5 F2@1.5 F2 E2 - F2 - G2 -@3")
 
-$: cat(
+_$: cat(
+  note(chords1),
+  note(chords2),
+  note(chords3),
+  note(chords4),
   note(chords5),
-  // note(chords2),
-  // note(chords3)
+  note(chords6)
   )
 .slow(4)
 .sound("piano")
-
